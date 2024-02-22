c0865937b220f2544c0f5b8bd0b7c310124bce5f
FerbaDev <elferbarron1@gmail.com>
2024-02-22 18:23:38 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+//ejercicio de mascotas y usuarios
+
+const express = require("express");
+const app = express();
+const PUERTO = 8080;
+
+//array de usuarios
+
+const users = [];
+
+//array de mascotas
+
+const pets = [];
+
+//recibir datos en formato json
+app.use(express.json());
+
+//rutas
+//rutas usuarios
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/api/users", (req, res) => {
+  const nuevoUsuario = req.body;
+  users.push(nuevoUsuario);
+  res.send({ status: "success", message: "Usuario creado correctamente!" });
+});
+//ruta mascotas
+
+app.get("/api/pets", (req, res) => {
+  res.json(pets);
+});
+
+//listen
+
+app.listen(PUERTO, () => {
+  console.log(`conectado en http://localhost:${PUERTO}`);
+});
